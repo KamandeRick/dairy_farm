217b9367697fbcdc6c605c238d9cd010104a29f6
kamanderick <124000425+KamandeRick@users.noreply.github.com>
2024-12-24 21:22:34 +0300

    update views to include create forms

--- farm_management/views.py
+++ farm_management/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Cow, MilkProduction, VeterinaryRecord
 from django.contrib.auth.decorators import login_required
+from .forms import CowForm, MilkProductionForm, VeterinaryRecordForm
 
-# Create your views here.
 #def index(request):
     #"""The home page for Farm Management"""
     #return render(request, 'farm_management/index.html')
@@ -53,3 +53,37 @@ def milk_production_list(request):
 def vet_record_list(request):
     records = VeterinaryRecord.objects.select_related('cow').order_by('-date')
     return render(request, 'farm_management/vet_record_list.html', {'records': records})
+
+# Create Views
+@login_required
+def add_cow(request):
+    if request.method == 'POST':
+        form = CowForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('farm_management:cow_list')
+    else:
+        form = CowForm()
+    return render(request, 'farm_management/cow_form.html', {'form': form})
+
+@login_required
+def add_milk_record(request):
+    if request.method == 'POST':
+        form = MilkProductionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('farm_management:milk_production_list')
+    else:
+        form = MilkProductionForm()
+    return render(request, 'farm_management/milk_production_form.html', {'form': form})
+
+@login_required
+def add_vet_record(request):
+    if request.method == 'POST':
+        form = VeterinaryRecordForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('farm_management:vet_record_list')
+    else:
+        form = VeterinaryRecordForm()
+    return render(request, 'farm_management/vet_record_form.html', {'form': form})
