25708ff870c577448f999533a2353be37488b026
kamanderick <124000425+KamandeRick@users.noreply.github.com>
2025-01-01 13:26:25 +0300

    add .is_active method to create inactive user until email is verified

--- farm_management/forms.py
+++ farm_management/forms.py
@@ -13,6 +13,7 @@ class CustomUserCreationForm(UserCreationForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data["email"]
+        user.is_active = False
         if commit:
             user.save()
         return user
