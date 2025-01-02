8fd0f0a09d150e3ec5d8dd11578663147be1baf0
kamanderick <124000425+KamandeRick@users.noreply.github.com>
2025-01-02 13:30:35 +0300

    Replace the SQLite DATABASES configuration with PostgreSQL

--- dairy_farm/settings.py
+++ dairy_farm/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
+
 # Load environment variables from .env file
 load_dotenv()
 
@@ -83,10 +85,10 @@ WSGI_APPLICATION = 'dairy_farm.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
 }
 
 # Auth settings
